8d47923754cc658e8235b0515f52b069fbbf905d
unknown <suraj.sakhare5869@gmail.com>
2024-09-25 19:04:25 +0530

    First Commit

---
+++ PalindromeNumbers/Models/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeNumbers.Models
+{
+    internal class Palindrome
+    {
+        public static string CheckPalindrome(int number)
+        {
+            int divisor = 10;
+            int temp = number;
+            int reverseNumber = 0;
+
+            //reverse the number
+            while (temp > 0)
+            {
+                int digit = temp % divisor;
+                temp /= divisor;
+                reverseNumber = reverseNumber * 10 + digit;
+            }
+
+            //Check the reverse number is equal to original number
+            if (reverseNumber == number)
+            {
+                return $"The entered number {number} is a Palindrome.";
+            }
+
+            return $"The entered number {number} is not a Palindrome.";
+        }
+    }
+}
---
+++ PalindromeNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using PalindromeNumbers.Models;
+
+namespace PalindromeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(Palindrome.CheckPalindrome(number));
+        }
+    }
+}
